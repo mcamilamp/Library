1136856ba7e78684e78c7dcbc882465ee9e398cf
mcamilamp <camimerpa@gmail.com>
2023-08-29 20:30:34 -0500

    Message

--- GUI.py
+++ GUI.py
@@ -10,32 +10,58 @@ def order_book():
         book = library.find_book_by_name(selected_book)
         if book:
             library.order_book(book.id)
-            messagebox.showinfo("Order", f"You ordered the book: {book.name}")
+            messagebox.showinfo("Order", f"Yes, the book: '{book.name}' is available \n\nBook Information:\n\nAuthor: '{book.author}' \nID: '{book.id}' \nGenre: '{book.genre}' ")
             entry.delete(0, END)
         else:
             messagebox.showinfo("Information", f"The book '{selected_book}' is not available")
+            
 
 window = Tk()
 window.title("Library: Under the Tree")
-window.geometry("500x500")
+window.geometry("560x600")
 window.configure(background="#cdd9ff")
+iconimage = PhotoImage(file="tree_489969.png")
+window.iconphoto(False, iconimage)
 
 title = Label(window, text="Under the Tree", font=('bold', 20))
-title.place(x=154, y=10)
+title.place(x=180, y=10)
 
 img = PhotoImage(file="tree.png")
 img = img.subsample(2)
 
 image_label = Label(window, image=img)
-image_label.place(x=125, y=60)
+image_label.place(x=150, y=60)
 
-entry_label = Label(window, text="Enter the name of the book you want to borrow:")
+# ///////////////////////////////////////////////////////////
+
+entry_label = Label(window, text="Enter the name of the book:")
 entry_label.place(x=110, y=340)
 
 entry = Entry(window)
 entry.place(x=110, y=370)
 
-button1 = Button(window, text="Order a book", command=order_book)
-button1.place(x=200, y=410)
+button1 = Button(window, text="Verify existence", command=order_book)
+button1.place(x=110, y=400)
 
+# //////////////////////////////////////////////////////////
+
+entry_label2 = Label(window, text="Show books by genre:")
+entry_label2.place(x=110, y=450)
+
+entry = Entry(window)
+entry.place(x=110, y=480)
+
+button1 = Button(window, text="Show Books", command=order_book)
+button1.place(x=110, y=508)
+
+# //////////////////////////////////////////////////////////
+
+entry_label2 = Label(window, text="Show books by Author:")
+entry_label2.place(x=310, y=450)
+
+entry = Entry(window)
+entry.place(x=310, y=480)
+
+button1 = Button(window, text="Show Books", command=order_book)
+button1.place(x=310, y=508)
 window.mainloop()
--- Module.py
+++ Module.py
@@ -1,8 +1,10 @@
 class Book:
-    def __init__(self, name, id, author):
+    def __init__(self, name, id, author, genre):
         self.name = name
         self.id = id
         self.author = author
+        self.genre = genre
+        self.available = True
 
 class Library:
     def __init__(self):
@@ -10,15 +12,26 @@ class Library:
         self.initializeBooks()
 
     def initializeBooks(self):
-        book0 = Book("To all the boys i loved Before", "000", "Jenny Han")
-        book1 = Book("Me before You", "001", "Jojo Moyes")
-        book2 = Book("Ugly Love", "002", "Collen Hoover")
-        book3 = Book("From Lukov, with Love", "003", "Mariana Zapata")
-        book4 = Book("The summer i turned Pretty", "004", "Jenny Han")
-        book5 = Book("Pride and Prejudice", "005", "Jane Austen")
-        book6 = Book("The notebook", "006", "Nicholas Sparks")
-        book7 = Book("It Ends with Us", "007", "Collen Hoover")
-        book8 = Book("The Love Hypothesis", "008", "Ali Hazelwood")
+        book0 = Book("To all the boys i loved Before", "000", "Jenny Han", "Romantic")
+        book1 = Book("Me before You", "001", "Jojo Moyes", "Romantic")
+        book2 = Book("The Selection", "002", "Kiera Cass", "Romantic")
+        book3 = Book("Little House on the Prairie ", "003", "Laura Ingalls Wilder", "Autobiography")
+        book4 = Book("Ugly Love", "004", "Collen Hoover", "Romantic")
+        book5 = Book("From Lukov, with Love", "005", "Mariana Zapata", "Romantic")
+        book6 = Book("The summer i turned Pretty", "006", "Jenny Han", "Romantic")
+        book7 = Book("Pride and Prejudice", "007", "Jane Austen", "Romantic")
+        book8 = Book("The notebook", "008", "Nicholas Sparks", "Romantic")
+        book9 = Book("It Ends with Us", "009", "Collen Hoover", "Romantic")
+        book10 = Book("The Love Hypothesis", "010", "Ali Hazelwood", "Romantic")
+        book11 = Book("The Diary of a Young Girl", "011", "Anne", "Autobiography")
+        book12 = Book("The Love Hypothesis", "012", "Ali Hazelwood", "Romantic")
+        book13 = Book("The One", "013", "Kiera Cass", "Romantic")
+        book14 = Book("Obsidian ", "014", "Jennifer L. Armentrout", "Fiction")
+        book15 = Book("Onyx", "015", "Jennifer L. Armentrout", "Fiction")
+        book16 = Book("Macondo", "016", "Gabriel Garcia Marquez", "Magical realism")
+        book17 = Book("Dracula", "017", "Bram Stoker", "terror")
+        book18 = Book("Frankenstein", "018", "Mary Shelley", "terror")
+
 
         self.add_book(book0)
         self.add_book(book1)
@@ -29,6 +42,18 @@ class Library:
         self.add_book(book6)
         self.add_book(book7)
         self.add_book(book8)
+        self.add_book(book9)
+        self.add_book(book10)
+        self.add_book(book11)
+        self.add_book(book12)
+        self.add_book(book13)
+        self.add_book(book14)
+        self.add_book(book15)
+        self.add_book(book16)
+        self.add_book(book17)
+        self.add_book(book18)
+
+
 
     def add_book(self, book):
         self.Books[book.id] = book
@@ -49,6 +74,33 @@ class Library:
             if book.name.lower() == book_name.lower():
                 return book
         return None
+    
+class Filter:
+    def filter(self, books: list[Book]):
+        return books
+
+class GenreFilter:
+    def __init__(self) -> None:
+        self.genre = genre 
+
+    def filter(self, books: list[Book]):
+        res = []
+        for book in books:
+            if book.genre == self.genre:
+                res.append(book)
+                return res 
+            
+class AuthorFilter:
+    def __init__(self) -> None:
+        self.author = AuthorFilter
+    
+    def filter(self, books: list[Book]):
+        res = []
+        for book in books:
+            if book.author == self.author:
+                res.append(book)
+                return res
 
 library = Library()
+
 
